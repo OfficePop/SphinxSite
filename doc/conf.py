a21ba41cc4324f7f74334d6c8a688c37badb0a65
Michael Benavidez <MBenavidez@gmail.com>
2020-06-10 19:15:50 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -3,3 +3,12 @@ project = "Calculate.Solutions"
 copyright = '2020, Calculate Solutions, Inc.'
 author = 'Calculate Solutions, Inc.'
 version = release = ''
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
